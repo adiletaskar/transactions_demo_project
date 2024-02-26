c361ca3e3c910e9fdb2830913da3957c162627aa
Adilet <adiletaskar1@gmail.com>
2024-02-26 12:22:11 +0600

    first

---
+++ src/main/java/com/example/demo/mapper/AccountRowMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+import com.example.demo.models.Account;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountRowMapper implements RowMapper<Account> {
+    @Override
+    public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Account a = new Account();
+        a.setId(rs.getInt("id"));
+        a.setName(rs.getString("name"));
+        a.setAmount(rs.getBigDecimal("amount"));
+        return a;
+    }
+}
---
+++ src/main/java/com/example/demo/models/Account.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Account {
+    private long id;
+    private String name;
+    private BigDecimal amount;
+}
